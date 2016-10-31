fc89672d721184a8ca9f579d17771fdd841fa01f
Katie O'Neill <katiemarieoneill@gmail.com>
2016-10-31 09:31:07 -0700

    Create method to return number of sodas in machine; Create method to find a soda given a brand

--- soda_machine.rb
+++ soda_machine.rb
@@ -6,12 +6,19 @@ class SodaMachine
     @cash = args[:cash]
   end
 
+# A soda machine will be able to return the number of sodas it contains.
   def current_inventory_count
+    sodas.length
   end
 
+# A soda machine will be able to find a single soda with a given brand.
   def find_soda(soda_brand)
+    sodas.find {|soda| soda.brand == soda_brand}
   end
 
+# A soda machine will be able to sell a soda with a given brand.
+# When a soda is sold, it should be removed from the soda machine
+#and the price of the soda should be added to the soda machine's cash
   def sell(soda_brand)
   end
 
