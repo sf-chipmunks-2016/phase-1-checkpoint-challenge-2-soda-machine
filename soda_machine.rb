c4926c1f8d5b460bf3b68fcc9f9a77197f7e8ed8
Parinaz Khosraviani <parinaz77@gmail.com>
2016-10-31 09:56:05 -0700

    completed class to keep track of sold sodas

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,33 @@ class SodaMachine
   end
 
   def current_inventory_count
+    count = 0
+    @sodas.each do |soda|
+      count +=1
+    end
+    count
   end
 
   def find_soda(soda_brand)
+    count = (0...sodas.length).to_a
+    count.each do |i|
+      if @sodas[i].brand == soda_brand
+        return sodas[i]
+      end
+    end
+    nil
   end
 
   def sell(soda_brand)
+    count = (0...sodas.length).to_a
+    count.each do |i|
+      if @sodas[i].brand == soda_brand
+        @cash += @sodas[i].price
+        sold=@sodas.delete(@sodas[i])
+        return sold
+      end
+    end
+    nil
   end
 
 end
