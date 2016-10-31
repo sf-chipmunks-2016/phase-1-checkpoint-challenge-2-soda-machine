274a551462f525baf9833cd0cb62ae503791c7b7
Ashley Humphrey <ashley.humphrey2@yahoo.com>
2016-10-31 10:04:46 -0700

    Tried to find the length of the machine

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,31 @@
+require_relative 'soda'
+
 class SodaMachine
-  attr_reader :sodas, :cash
+  attr_reader :sodas, :cash, :args
 
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    @args = args
   end
 
   def current_inventory_count
+    #returns the number of sodas in the machine
+    # soda_machine is a hash with a key :sodas that is a array
+    #this statment returns 4 put I believe it is counting something else.
+     p @args[:sodas].length
+    # p @machine[@sodas].[@brand]
   end
 
   def find_soda(soda_brand)
+    # A soda machine will be able to find a single soda with a given brand. 
+    p @args[:sodas][soda_brand]
   end
 
   def sell(soda_brand)
   end
 
 end
+
+# SodaMachine.new(sodas: [pepsi, mountain_dew, coke_zero, second_pepsi], cash: 1.00) 
+# :sodas=>[#<Soda:0x007fbcf92985c0 @brand="Pepsi", @price=0.65>, #<Soda:0x007fbcf92983e0 @brand="Mount... @brand="Coke Zero", @price=1.0>, #<Soda:0x007fbcf92dbeb0 @brand="Pepsi", @price=0.65>], :cash=>1.0}
