db0cd85a445bf0157028f064b934fbff838e56fe
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-10-31 10:02:42 -0700

    Add code for checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -4,15 +4,27 @@ class SodaMachine
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+
   end
 
   def current_inventory_count
+    # returns the count of the sodas
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    # I want to find a way to iterate through the has and find a match
+    # I wanted to call the key by matching it to the 
+    soda = soda_brand.downcase.gsub(/ /, "_")
+    @sodas.select {|k,v| k == soda.to_sym}
+
+
   end
 
   def sell(soda_brand)
+    # If nil then return nil
+    # if sold drop that exact one sold
   end
 
 end
+
