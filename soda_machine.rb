381e12b45111a58122733004a366c75782894299
Techforchange <arleneperezsb@gmail.com>
2016-10-31 09:32:50 -0700

    Finds the soda the user asked for

--- soda_machine.rb
+++ soda_machine.rb
@@ -5,11 +5,20 @@ class SodaMachine
     @sodas = args[:sodas]
     @cash = args[:cash]
   end
-
+#A soda machine will be able to return the number of sodas it contains. 
+#This method should be called SodaMachine#current_inventory_count.
   def current_inventory_count
+    return soda_machine.length
   end
 
   def find_soda(soda_brand)
+    sodas.select do |soda| 
+      if soda == soda_brand
+        return soda
+      elsif soda != soda_brand
+        return nil
+      end
+    end
   end
 
   def sell(soda_brand)
