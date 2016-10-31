4f58d87821674e1d9cc512ba5f69712edae18dcf
meganeding1git config --global user.email eding.megan@gmail.comgit config --global core.editor subl <eding.megan@gmail.com>
2016-10-31 09:36:50 -0700

    modify rspec and soda_machine files to get current count in soda machine

--- soda_machine.rb
+++ soda_machine.rb
@@ -4,15 +4,20 @@ class SodaMachine
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    p args 
   end
 
   def current_inventory_count
+    # count = args[:sodas].length 
+    # return the number of sodas it contains
   end
 
   def find_soda(soda_brand)
+    # find a single soda with a given brand
   end
 
   def sell(soda_brand)
+     # A soda machine will be able to sell a soda with a given brand. When a soda is sold, it should be removed from the soda machine and the price of the soda should be added to the soda machine's cash
   end
 
 end
