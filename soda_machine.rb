124ead2caf845e84c64ba2dd6e6fa25af78a9bf9
alealejandro <alex.m.4155@gmail.com>
2016-10-31 10:00:21 -0700

    Create methods that:   - return the number of sodas in machine: current_inventory_count   - sell a soda     - return the soda to sell     - add price of soda to sell to machine cash     - remove soda to sell from machine inventory

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,39 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
+  attr_accessor :current_inventory_count
+  
 
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
-  end
-
-  def current_inventory_count
+    @current_inventory_count = @sodas.length
   end
 
   def find_soda(soda_brand)
+    i = 0
+    while i < @sodas.length
+      if @sodas[i].brand == soda_brand 
+        @sodas[i]
+        break
+      end
+      i += 1
+    end
+    
+    @sodas[i]
+    # returns either match or nil 
   end
 
   def sell(soda_brand)
+    soda_to_sell = find_soda(soda_brand)
+
+    if soda_to_sell != nil
+      @cash += soda_to_sell.price 
+      @sodas.delete(soda_to_sell)
+    end
+
+    soda_to_sell
   end
 
 end
