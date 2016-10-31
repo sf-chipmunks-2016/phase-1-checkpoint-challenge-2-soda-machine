8442739b9e49afbda9aa1c6f398fab78b5fc8392
Christian Acuña <cacuna0828@gmail.com>
2016-10-31 09:34:01 -0700

    implement #sell and pass tests

--- soda_machine.rb
+++ soda_machine.rb
@@ -15,6 +15,14 @@ class SodaMachine
   end
 
   def sell(soda_brand)
+    removed_soda = nil
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        removed_soda = @sodas.delete(soda)
+        @cash += soda.price
+      end
+    end
+    removed_soda
   end
 
 end
