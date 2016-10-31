e841688c708c76fdc305994ba327a9355b549908
agonzalez0515 <agonzalez0515@gmail.com>
2016-10-31 10:03:21 -0700

    checkpoint challenge attempt

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    #@sodas.find {|name| name = soda_brand}
+    #@sodas.select {|name| name = soda_brand}
+    #@sodas.detect{|name| name = soda_brand}
+    #@sodas.index(soda_brand)
+    #these are all the methods I attempted to use, but none passed the tests.
   end
 
   def sell(soda_brand)
+  #@sodas.delete(soda_brand)
+  # @sodas.reject(soda_brand)
+  # I want a method that will return the array without the deleted soda
+  #  @cash += @sodas[:price]
   end
 
 end
