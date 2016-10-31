8651cd414d9f209c208bc8f58e809a6ee0d6c800
collin shelby <email": null>
2016-10-31 09:36:46 -0700

    phase 1 checkpoint challenge complete

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,5 +1,7 @@
 class SodaMachine
-  attr_reader :sodas, :cash
+
+  attr_reader :sodas
+  attr_accessor :cash
 
   def initialize(args = {})
     @sodas = args[:sodas]
@@ -7,12 +9,20 @@ class SodaMachine
   end
 
   def current_inventory_count
+    self.sodas.count()
   end
 
   def find_soda(soda_brand)
+    self.sodas.find { |soda| return soda if soda.brand == soda_brand }
   end
 
   def sell(soda_brand)
+    self.sodas.find do |soda|
+      if soda.brand == soda_brand
+        self.cash += soda.price
+        self.sodas.delete(soda)
+      end
+    end
   end
 
 end
