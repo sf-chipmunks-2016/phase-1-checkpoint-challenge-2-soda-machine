1d1c44c874d359b7077e02ca65dd330cf61353d3
Sophia Varella <sophiavarella@gmail.com>
2016-10-31 09:58:41 -0700

    Update driver code to the file

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,4 +1,4 @@
-require_relative "soda"
+# require_relative "soda"
 
 class SodaMachine
   attr_reader :sodas, :cash
@@ -27,5 +27,19 @@ class SodaMachine
     soda
   end
 end
+
+
+# p pepsi = Soda.new(brand: 'Pepsi', price: 0.65) 
+# p coke_zero = Soda.new(brand: 'Coke_Zero', price: 1.00)
+# new_machine = SodaMachine.new(sodas: [pepsi, coke_zero], cash: 1.00)
+# p new_machine.sodas
+# p new_machine.cash
+# p new_machine.current_inventory_count
+# puts "After a sale"
+# p new_machine.find_soda("Pepsi")
+# p new_machine.sell("Pepsi")
+# p new_machine.sodas
+# p new_machine.cash
+# p new_machine.current_inventory_count
 
 
